f3d2d392718e57713100feda41bf0fd6c7a11844
Diego Costantino <diegocstn@gmail.com>
2014-05-22 14:46:13 +0200

    added words counter

--- diego/06_functional_frequencies.py
+++ diego/06_functional_frequencies.py
@@ -2,7 +2,7 @@ import re
 def count_frequencies(filetext):
   """ count words occurence for the file passed by parameter"""
   f = open(filetext,"r")
-  count_frequencies_for_line(f,{})
+  count_frequencies_for_line(f,{},0)
 
 def word_is_tracked(word,mem):
   """ check if word is still present in mem """
@@ -21,13 +21,16 @@ def check_words(words,index,mem):
   return check_words(words,index+1,mem)
 
 
-def count_frequencies_for_line(f,mem):
+def count_frequencies_for_line(f,mem,counter):
   line = f.readline()
   if( not line == '' ):
     words = re.split("\W+",line)
+    del words[-1] # remove last item
+    counter += len(words)
     mem   = check_words(words,0,mem)
-    count_frequencies_for_line(f,mem)
+    count_frequencies_for_line(f,mem,counter)
   else:
+    print( "{0} words founded".format(counter))
     print(mem)
 
 
